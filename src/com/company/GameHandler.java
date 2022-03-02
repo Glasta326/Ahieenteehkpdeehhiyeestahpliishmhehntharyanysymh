b88fb3f8cee92a7d8fb880580633be63db8c9b5c
JakeRees <jakerees04@gmail.com>
2022-03-02 10:00:07 +0000

    Created game handler

--- src/com/company/GameHandler.java
+++ src/com/company/GameHandler.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+import java.lang.Integer;
+
 public class GameHandler {
-
+    static int playerCount = 0;
     public static void main(String[] args) {
-
+        initialise();
     }
 
+    public static void initialise(){
+        Scanner scanner = new Scanner(System.in);
+        playerCount = Integer.parseInt(scanner.nextLine());
+        System.out.println(playerCount);
+        for (int i = 0; i < playerCount; i++) {
+
+        }
+    }
 }
