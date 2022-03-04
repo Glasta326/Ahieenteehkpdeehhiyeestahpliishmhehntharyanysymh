0d8d5893e6a0ebe0c2a079c2255017517097a210
Glasta <number2048.12@gmail.com>
2022-03-04 11:45:08 +0000

    updat4ed tile and player

--- src/com/main/Player.java
+++ src/com/main/Player.java
@@ -1,7 +1,31 @@
 package com.main;
 
+import java.util.Random;
+
 public class Player
 {
+    private int food;
+    private Tile currentTile;
+    private String animal;
+    private int coolrandomfx;
+
+    public int Rolldice() {
+        Random ran = new Random();
+        for (int i = 0; i < 20; i++) {
+            ran = new Random();
+            coolrandomfx = ran.nextInt(1) + 6;
+        }
+        return coolrandomfx;
+    }
+
+    public void BuyArea(Tile currentTile){
+        //currentTile.owner = player.Id
+        //do shit here
+    }
+
+    public void IncreasePopulation(){
+        currentTile.population += 1; //replace 1 with actual amount
+    }
 
     public Player() {
 
--- src/com/main/Tile.java
+++ src/com/main/Tile.java
@@ -4,12 +4,19 @@ public class Tile {
     private int x;
     private int y;
     private int index;
+    int owner = 0;
+    int population;
     Vector2 position;
 
-    public Tile(int x, int y, int index) {
+
+    public Tile(int x, int y, int index,int owner, int population) {
         this.position = new Vector2(x,y);
         this.index = index;
+        this.owner = owner;
+        this.population = population;
     }
+
+
 
 
 }
